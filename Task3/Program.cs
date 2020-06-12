70ba52523edb4095f6e9196ed5fc2819d674f778
Vadim Khamlyuk <vadim.khamlyuk@gmail.com>
2020-06-13 00:44:39 +0300

    init commit

---
+++ Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Task3
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            if (args.Length % 2 == 0 || args.Length < 3)
+                Console.WriteLine("Error, otsosi");
+            Random rnd = new Random();
+            int pcMove = rnd.Next(args.Length);
+            Console.WriteLine(pcMove);
+            bool isInt = false;
+            int myMove;
+            do
+            {
+                ShowMenu(args);
+            } while (!int.TryParse(Console.ReadLine(), out myMove) || (myMove < 1 || myMove > args.Length));
+
+            //Console.WriteLine(args[pcMove]);
+            Console.ReadLine();
+        }
+
+        private static void ShowMenu(string[] args)
+        {
+            Console.WriteLine("Available moves:");
+            for (int i = 0; i < args.Length; i++)
+                Console.WriteLine(String.Format("{0} - {1}", i + 1, args[i]));
+            Console.WriteLine("0 - exit");
+            Console.Write("Enter your move:");
+        }
+    }
+}
